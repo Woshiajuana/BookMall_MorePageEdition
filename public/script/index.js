8c99cdf1b3be8d6df0bfc2f0286d04e8022393ed
Woshiajuana <chenzhigang@2144.cn>
2017-03-02 18:48:09 +0800

    首页用户登录功能初步完成

--- public/script/index.js
+++ public/script/index.js
@@ -1,4 +1,40 @@
 /**
  * Created by 2144 on 2017/3/1.
  */
-var vm = new Vue({});
+var Nav = new Vue({
+    el: '#navBar',
+    data: {
+        is_login: false,
+        user_name: ''
+    },
+    mounted: function () {
+        this.$nextTick(function () {
+            this.init();
+        });
+    },
+    methods: {
+        //初始化方法
+        init: function () {
+            this.isLogin();
+        },
+        //判断用户是否已经登录
+        isLogin: function () {
+            //获取sessionStorage的用户信息
+            var user = sessionStorage.getItem('user');
+            //如果不存在，则用户还未登录
+            if(!user) return;
+            this.is_login = true;
+            user = JSON.parse(user);
+            this.user_name = user.user_name;
+        },
+        //用户安全退出的方法
+        loginOut: function () {
+            this.is_login = false;
+            this.deleteUserForSessionStorage();
+        },
+        //删除HTML5本地缓存中的用户信息
+        deleteUserForSessionStorage: function () {
+            sessionStorage.removeItem('user');
+        }
+    }
+});
